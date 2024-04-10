ddc803230c3e8f0378890e7d73b9f31a803f675b
Nguyen Tuan <norsinlight@gmail.com>
2024-04-10 17:06:54 +0700

    Add OderStatusDto class

---
+++ SinStore [Server]/src/main/java/com/api/SinStore/dtos/OderStatusDto.java
@@ -0,0 +1,15 @@
+package com.api.SinStore.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OderDto {
+
+    private String status;
+}
